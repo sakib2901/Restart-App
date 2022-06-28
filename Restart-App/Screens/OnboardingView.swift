bfc7808caa68ead9c96af3a31cd40bddf5c073f5
Sakib Bin Alim <sakib.alim@bjitgroup.com>
2022-06-28 14:57:39 +0600

    Setting up project.
     - Added .gitignore.
     - Changed launch screen color.
     - Disabled landscape mode.

---
+++ Restart-App/Screens/OnboardingView.swift
@@ -0,0 +1,20 @@
+//
+//  OnboardingView.swift
+//  Restart-App
+//
+//  Created by Sakib Bin Alim on 28/6/22.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
