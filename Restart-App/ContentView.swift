def16e37c9cd5298373d13e3bfeeb24c7b9a766c
BJIT <sakib.alim@bjitgroup.com>
2022-06-28 13:08:29 +0600

    Initial Commit

---
+++ Restart-App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Restart-App
+//
+//  Created by Sakib Bin Alim on 28/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
