904dbe2d2339867113ed5dd13c7bf9a76779fb6e
Sakib Bin Alim <ankon.sba22@gmail.com>
2022-07-01 13:08:30 +0600

    Added a sliding button UI
     - Designed the sliding button UI
     - Added onTapGesture on the sliding button UI

--- Restart-App/Screens/OnboardingView.swift
+++ Restart-App/Screens/OnboardingView.swift
@@ -57,6 +57,49 @@ struct OnboardingView: View {
                 Spacer()
                 
                 // Mark: - FOOTER
+                
+                ZStack {
+                    Capsule()
+                        .fill(.white.opacity(0.2))
+                    
+                    Capsule()
+                        .fill(.white.opacity(0.2))
+                        .padding(8)
+                    
+                    Text("Get Started")
+                        .font(.system(.title3, design: .rounded))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .offset(x: 20)
+                    
+                    HStack {
+                        Capsule()
+                            .fill(Color("ColorRed"))
+                            .frame(width: 80)
+                        Spacer()
+                    }
+                    
+                    HStack {
+                        ZStack {
+                            Circle()
+                                .fill(Color("ColorRed"))
+                            Circle()
+                                .fill(.black.opacity(0.15))
+                                .padding(8)
+                            Image(systemName: "chevron.right.2")
+                                .font(.system(size: 24, weight: .bold))
+                        }
+                        .foregroundColor(.white)
+                        .frame(width: 80, height: 80, alignment: .center)
+                        .onTapGesture {
+                            isOnboardingViewActive = false	
+                        }
+                        
+                        Spacer()
+                    }
+                } //: FOOTER
+                .frame(height: 80, alignment: .center)
+                .padding()
             }
         }
     }
