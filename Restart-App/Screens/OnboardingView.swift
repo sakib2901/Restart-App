1bee874f479efb7f30bea0e0e864b07edc898304
Sakib Bin Alim <ankon.sba22@gmail.com>
2022-07-01 11:59:35 +0600

    Making the Onboardin View
     - Added Header & Center section

--- Restart-App/Screens/OnboardingView.swift
+++ Restart-App/Screens/OnboardingView.swift
@@ -11,12 +11,52 @@ struct OnboardingView: View {
     @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
     
     var body: some View {
-        VStack(spacing: 20) {
-            Text("Onboarding")
-                .font(.largeTitle)
-            
-            Button("Start") {
-                isOnboardingViewActive = false
+        ZStack {
+            Color("ColorBlue")
+                .ignoresSafeArea(.all, edges: .all)
+            VStack(spacing: 20) {
+                // Mark: - HEADER
+                
+                Spacer()
+                
+                VStack(spacing: 0) {
+                    Text("Share.")
+                        .font(.system(size: 60))
+                        .foregroundColor(.white)
+                        .fontWeight(.heavy)
+                    
+                    Text("""
+                    It's not how much we give but
+                    how much love we put into giving.
+                    """)
+                    .font(.title3)
+                    .fontWeight(.light)
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 10)
+                } //: HEADER
+                
+                // Mark: - CENTER
+                
+                ZStack {
+                    ZStack {
+                        Circle()
+                            .stroke(.white.opacity(0.2), lineWidth: 40)
+                            .frame(width: 260, height: 260, alignment: .center)
+                        
+                        Circle()
+                            .stroke(.white.opacity(0.2), lineWidth: 80)
+                            .frame(width: 260, height: 260, alignment: .center)
+                    }
+                    
+                    Image("character-1")
+                        .resizable()
+                        .scaledToFit()
+                } //: CENTER
+                
+                Spacer()
+                
+                // Mark: - FOOTER
             }
         }
     }
